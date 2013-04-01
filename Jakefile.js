bf370d086c2847f2ec02c792d1d78fa81ea13fca
Phil Booth <pmbooth@gmail.com>
2013-04-01 21:06:29 +0100

    Update minified script.

--- Jakefile.js
+++ Jakefile.js
@@ -1,77 +1,75 @@
 /*jshint nomen:false */
 /*globals require, console, complete, __dirname, process */
 
-(function () {
-    'use strict';
+'use strict';
 
-    var exec = require('child_process').exec,
+var exec = require('child_process').exec,
 
-    commands = {
-        minify: './node_modules/.bin/uglifyjs ./src/check-types.js --compress --mangle --output ./src/check-types.min.js',
-        test: './node_modules/.bin/mocha --ui tdd --reporter spec --colors --slow 50 ./test/check-types.js',
-        lint: './node_modules/.bin/jshint ./src/check-types.js --config config/jshint.json',
-        prepare: 'npm install'
-    };
+commands = {
+    minify: './node_modules/.bin/uglifyjs ./src/check-types.js --compress --mangle --output ./src/check-types.min.js',
+    test: './node_modules/.bin/mocha --ui tdd --reporter spec --colors --slow 50 ./test',
+    lint: './node_modules/.bin/jshint ./src/check-types.js --config config/jshint.json',
+    prepare: 'npm install'
+};
 
-    desc('Minify the source code for deployment.');
-    task('minify', [ 'prepare', 'lint', 'test' ], function () {
-        runTask(minify, 'Minifying...');
-    }, {
-        async: true
+desc('Minify the source code for deployment.');
+task('minify', function () {
+    runTask(minify, 'Minifying...');
+}, {
+    async: true
+});
+
+desc('Run the unit tests.');
+task('test', function () {
+    runTask(test, 'Testing...');
+}, {
+    async: true
+});
+
+desc('Lint the source code.');
+task('lint', function () {
+    runTask(lint, 'Linting...');
+}, {
+    async: true
+});
+
+desc('Install dependencies.');
+task('prepare', function () {
+    runTask(prepare, 'Preparing the build environment...');
+}, {
+    async: true
+});
+
+function runTask (operation, message) {
+    console.log(message);
+    operation();
+}
+
+function minify () {
+    runCommand(commands.minify);
+}
+
+function test () {
+    runCommand(commands.test);
+}
+
+function lint () {
+    runCommand(commands.lint);
+}
+
+function prepare () {
+    runCommand(commands.prepare);
+}
+
+function runCommand (command) {
+    exec(command, { cwd: __dirname }, function (error, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        if (typeof error === 'object' && error !== null) {
+            console.log(error.message);
+            process.exit(1);
+        }
+        complete();
     });
-
-    desc('Run the unit tests.');
-    task('test', [ 'prepare' ], function () {
-        runTask(test, 'Testing...');
-    }, {
-        async: true
-    });
-
-    desc('Lint the source code.');
-    task('lint', [ 'prepare' ], function () {
-        runTask(lint, 'Linting...');
-    }, {
-        async: true
-    });
-
-    desc('Install dependencies.');
-    task('prepare', function () {
-        runTask(prepare, 'Preparing the build environment...');
-    }, {
-        async: true
-    });
-
-    function runTask (operation, message) {
-        console.log(message);
-        operation();
-    }
-
-    function minify () {
-        runCommand(commands.minify);
-    }
-
-    function test () {
-        runCommand(commands.test);
-    }
-
-    function lint () {
-        runCommand(commands.lint);
-    }
-
-    function prepare () {
-        runCommand(commands.prepare);
-    }
-
-    function runCommand (command) {
-        exec(command, { cwd: __dirname }, function (error, stdout, stderr) {
-            console.log(stdout);
-            console.log(stderr);
-            if (typeof error === 'object' && error !== null) {
-                console.log(error.message);
-                process.exit(1);
-            }
-            complete();
-        });
-    }
-}());
+}
 
